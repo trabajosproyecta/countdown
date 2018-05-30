fbc021cf434fa06ead9ed4c32de5e3ab422089e9
ironcadiz <afcadiz@uc.cl>
2018-05-30 17:05:52 -0400

    fixed plural on clock

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@
 
 		$('#clock-countdown').countdown('2018/06/06 13:00:00').on('update.countdown', function(event) {
 			var $this = $(this).html(event.strftime(''
-				+ '<div class="counter-container"><div class="counter-box first"><div class="number">%-D</div><span>Días%!d</span></div>'
+				+ '<div class="counter-container"><div class="counter-box first"><div class="number">%-D</div><span>Día%!d</span></div>'
 				+ '<div class="counter-box"><div class="number">%H</div><span>Horas</span></div>'
 				+ '<div class="counter-box"><div class="number">%M</div><span>Minutos</span></div>'
 				+ '<div class="counter-box last"><div class="number">%S</div><span>Segundos</span></div></div>'
