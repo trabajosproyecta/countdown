c8e40503805bdd7b8e3b8beedc65248113a553d4
ironcadiz <afcadiz@uc.cl>
2018-06-05 19:28:00 -0400

    added inscription url

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
 // Hora de las inscripciones
-const inscription_string ='2018/06/05 13:30:00'
+const inscription_string ='2018/06/06 13:30:00'
 //link inscripciones.
-const inscription_link ='https://google.com'
+const inscription_link ='https://goo.gl/forms/8gfypiA1eYVvUMpt1'
